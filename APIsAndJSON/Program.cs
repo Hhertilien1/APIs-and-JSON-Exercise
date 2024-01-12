80c06ab083fd777be5250f3f93b0fc2ab50f0b9a
Hhertilien1 <kinghertilien@gmail.com>
2024-01-12 10:28:38 -0500

    DONE PART 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            var client = new HttpClient();
+
+            var kanyeAPi = "https://api.kanye.rest";
+
+            var ronSwanApi = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            for (int i = 0; i <= 5; i++)
+            {
+                var ronResponse = client.GetStringAsync(ronSwanApi).Result;
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Replace('"', ' ').Trim();
+
+                Console.WriteLine($"Ron: {ronQuote}");
+                Console.WriteLine();
+
+                var KanyeResponse = client.GetStringAsync(kanyeAPi).Result;
+                var KanyeQuote = JObject.Parse(KanyeResponse).GetValue("quote").ToString();
+
+                Console.WriteLine($"Kanye: {KanyeQuote}");
+                Console.WriteLine();
+            }
         }
     }
 }
